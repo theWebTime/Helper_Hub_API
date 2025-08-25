f036346f42b5b245c2d51167d60971192daabe51
Darshak0891 <darshakpatel02@gmail.com>
2025-08-25 12:23:45 +0530

    Site Setting Issue Fix

--- app/Http/Controllers/API/SiteSettingController.php
+++ app/Http/Controllers/API/SiteSettingController.php
@@ -68,7 +68,7 @@ class SiteSettingController extends BaseController
     {
         // dd(1);
         try {
-            $data = SiteSetting::select('id', 'razorpay_key_id', 'platform_fee_value', 'cancellation_charges', 'company_name', 'logo', 'address', 'phone', 'email', 'about_us', 'terms_conditions', 'privacy_policy', 'refund_policy', 'support_email', 'support_phone', 'facebook_url', 'instagram_url', 'twitter_url', 'linkedin_url', 'youtube_url', 'whatsapp_number', 'telegram_url', 'pinterest_url', 'tiktok_url', 'snapchat_url', 'threads_url')->first();
+            $data = SiteSetting::select('id', 'razorpay_key_id', 'razorpay_key_secret', 'platform_fee_value', 'cancellation_charges', 'company_name', 'logo', 'address', 'phone', 'email', 'about_us', 'terms_conditions', 'privacy_policy', 'refund_policy', 'support_email', 'support_phone', 'facebook_url', 'instagram_url', 'twitter_url', 'linkedin_url', 'youtube_url', 'whatsapp_number', 'telegram_url', 'pinterest_url', 'tiktok_url', 'snapchat_url', 'threads_url')->first();
             if (is_null($data)) {
                 return $this->sendError('Site Setting not found.');
             }
